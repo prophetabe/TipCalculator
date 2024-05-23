36a38a3e2752e6a373f08459bbfedbc87de95a2c
prophetabe <iaderoju8@gmail.com>
2024-05-22 22:16:54 -0400

    Update cc_1.js

--- cc_1.js
+++ cc_1.js
@@ -4,18 +4,18 @@ console.log(bill,bill*result,bill+bill*result)
 for (let i = 0; i < 10; i++) {
 	console.log(i);
 }
-// commit 1 
+// commit 1 Initial logic for calculating tip using ternary operator.
 bill=100;
 tip = bill >= 50 && bill <=300?0.2:0.15
 console.log(bill,bill*result,bill+bill*result)
 
 // start of program
-// commit 3
+// commit 3 Create and test calcTip function.
 const bills1 = [275, 40, 430]; // we know the bill
 const bills2 = [125, 555, 44]; // ^
 let tips = []; // empty because we don't know the tips yet.
 let totals = []; // ^
-// commit 2
+// commit 2 Add output functionality to display bill, tip, and total.
 function calcTip(bill) { 
     tip = bill >= 50 && bill <=300?0.2:0.15
     calc = tip * bill;
@@ -24,7 +24,7 @@ function calcTip(bill) {
     //              0     1   2
     // data_set1 = [275, 40, 430]
 
-// commit 4
+// commit 4 Implement and populate arrays for bills, tips, and totals.
 var size = 3;
 for ( let i = 0; i< 3; i++) {
     //console.log(i);                      // 0   1   2
